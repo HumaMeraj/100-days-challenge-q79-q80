432c939902f5c907f211aad7884fd8d0b01092f3
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-18 00:30:58 +0500

    File of q80

---
+++ q80.js
@@ -0,0 +1,12 @@
+//Updating Object Properties: Add a property named color to the existing car object, and then update the year property to 2021. Show how to perform these operations.
+// Starting with  car object
+var car = {
+    make: "Honda",
+    model: "City",
+    year: "2021",
+    color: "blue"
+};
+// Adding a new property 'color' and updating 'year'
+car.color = "black"; // Adds a new property 'color'
+car.year = "2022"; // Updates the 'year' property
+console.log(car);
