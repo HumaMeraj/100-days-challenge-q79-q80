0bfde016bc0d8747aff06c94c42b667653892655
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-18 00:30:27 +0500

    File of q79

---
+++ q79.js
@@ -0,0 +1,7 @@
+// Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. 
+var car = {
+    make: "Honda",
+    model: "Civic",
+    year: "2022"
+};
+console.log(car.model); // Outputs: Civic
---
+++ q79.ts
@@ -0,0 +1,10 @@
+// Creating and Accessing Object Properties: Construct an object representing a car with properties for make, model, and year. 
+
+let car = {
+    make:"Honda",
+    model:"Civic",
+    year:"2022"
+};
+console.log(car.model);     // Outputs: Civic
+
+// We use dot notation (car.model) to get the model of the car from our object.
